10c761f8f51cbe5e49036d3410d8ad42159e4ddf
eman <eman@onet.eu>
2017-07-22 13:39:07 +0200

    ok 244

--- Main3.java
+++ Main3.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class Main3 {
     public static void main(String[] args) {
-        Set<String> names = new HashSet<>;
+        Set<String> names = new HashSet<>();
         names.add("Oskar");
         names.add("1234");
         names.add("Oskar");
 
         for (String name : names) {
-
+            System.out.println(name);
         }
     }
 }
