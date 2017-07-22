2f37b721c72c5eede868adaae40b5742583ed398
eman <eman@onet.eu>
2017-07-22 13:35:26 +0200

    ok 2

---
+++ Main3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>;
+        names.add("Oskar");
+        names.add("1234");
+        names.add("Oskar");
+
+        for (String name : names) {
+
+        }
+    }
+}
